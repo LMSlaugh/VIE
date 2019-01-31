4d28bc211715142765c5db6d19afac39045c18e9
Lisa Slaughter <lisa.m.slaughter@gmail.com>
2019-01-31 08:55:27 -0800

    Fixed list initialization issue on while loop iteration. Began to code ability to handle historical data (what about timestamps? Can require interpolation for now....)

--- main-historical.py
+++ main-historical.py
@@ -47,22 +47,26 @@ def FuseVacancyTimestamps(timestamps):
 output = pd.DataFrame(index=[0],columns=["runtimedt","overallprobadt","overallprobaval","wifidt","wifival","wifiproba","co2dt","co2val","co2proba","elecdt","elecval","elecproba"])
 sensors = CreateVirtualSensors("VIE-sensor-metadatabase.csv") # Read sensors from file, instantiate. If missing data, fill with dummy data ("") and move on to next sensor
 
-while True:
+
+
+for sensor in sensors:
     probabilities = []
     timestamps = []
-    for sensor in sensors:
-        sensor.UpdateSnapshot()
-        sensor = sensor.PreprocessData() # Currently does nothing
+    sensordata = sensor.ObtainHistoricalData() # into a dataframe for that sensor
+    sensor = sensor.PreprocessData() # Currently does nothing
 
-        if sensor.vacancyrelationship == 0 : # In case of sigmoid, check if needed parameters exist. If not, calculate them
-            p1_nanflag = math.isnan(sensor.param1)
-            p2_nanflag = math.isnan(sensor.param2)
-            if p1_nanflag | p2_nanflag : # if any parameters are nan, calculate them
-                sensor = sensor.BuildVacancyRelationship() # Does nothing for now. May need separate relationship builders for each function type. Need to test this program flow.
+    #if sensor.vacancyrelationship == 0 : # In case of sigmoid, check if needed parameters exist. If not, calculate them
+        #p1_nanflag = math.isnan(sensor.param1)
+        #p2_nanflag = math.isnan(sensor.param2)
+        #if p1_nanflag | p2_nanflag : # if any parameters are nan, calculate them
+            #sensor = sensor.BuildVacancyRelationship() # Does nothing for now. May need separate relationship builders for each function type. Need to test this program flow.
     
-        sensor.PredictVacancyProbability()
-        timestamps.append(sensor.snapshottimestamp) # Capture datetime in a list
-        probabilities.append(sensor.vacancyprobability) # Capture datetime in a list
+for index, row in sensordata.iterrows():
+    sensor.snapshottimestamp = row[0]
+    sensor.snapshotvalue = row[1]
+    sensor.PredictVacancyProbability()
+    timestamps.append(sensor.snapshottimestamp) # Capture datetime in a list
+    probabilities.append(sensor.vacancyprobability) # Capture datetime in a list
 
     # fuse predictions for all sensors
     overallprobabilityvalue = FuseVacancyProbabilities(probabilities)
@@ -75,8 +79,6 @@ while True:
     elec = sensors[2]
     output.iloc[0] = [dt.datetime.now(), overallprobabilitytimestamp, overallprobabilityvalue, wifi.snapshottimestamp, wifi.snapshotvalue, wifi.vacancyprobability, co2.snapshottimestamp, co2.snapshotvalue, co2.vacancyprobability, elec.snapshottimestamp, elec.snapshotvalue, elec.vacancyprobability]
     output.to_csv("VacancyPredictionTestFile.csv", mode="a", header=False, index=False)
-    sleepminutes = 5
-    time.sleep(sleepminutes*60)
 
 
 thisisastopgap = "stopgap"
